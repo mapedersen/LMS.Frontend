8299cc8120d005e144d4342b188e9dd2a9eba36d
marcus pedersen <marcus@mpedersen.me>
2024-09-26 17:01:48 +0200

    Init TeacherDashboard component

---
+++ src/components/TeacherDashboard.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const TeacherDashboard = () => {
+  return (
+    <Box p={5}>
+      <Heading as="h2">Welcome, Teacher!</Heading>
+      {/* Additional teacher-specific components */}
+    </Box>
+  );
+};
+
+export default TeacherDashboard;
