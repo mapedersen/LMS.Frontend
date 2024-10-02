1ba7024a476a428af455310a3aee25f158cda281
marcus pedersen <marcus@mpedersen.me>
2024-10-02 11:01:30 +0200

    Refactor

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,31 +1,10 @@
-import { jwtDecode } from "jwt-decode";
-import { CourseDetails } from "../types/course";
-import { refreshAccessToken } from "./authService";
-
-// Check if the token is expired
-const checkIfTokenExpired = (accessToken: string) => {
-  if (!accessToken) return true; // Token doesn't exist, treat as expired
-
-  const decodedToken: Record<string, any> = jwtDecode(accessToken);
-  const currentTime = Date.now() / 1000;
-  return decodedToken.exp < currentTime;
-};
+import { Course, Courses } from "../types/course";
 
 // Fetch course details
 export const fetchCourseDetails = async (
   accessToken: string,
   userRole: string
-): Promise<CourseDetails[]> => {
-  // Check if token has expired
-  if (checkIfTokenExpired(accessToken)) {
-    const { accessToken: newAccessToken } = await refreshAccessToken();
-    if (newAccessToken) {
-      accessToken = newAccessToken; // Update accessToken to the newly fetched one
-    } else {
-      throw new Error("Failed to refresh access token, please log in again.");
-    }
-  }
-
+): Promise<Course | Courses> => {
   const url =
     userRole === "Student"
       ? "https://localhost:7243/api/courses/student"
@@ -35,12 +14,13 @@ export const fetchCourseDetails = async (
     method: "GET",
     headers: {
       Authorization: `Bearer ${accessToken}`,
-      "content-Type": "application/json",
+      "Content-Type": "application/json",
     },
   });
 
   if (!response.ok) {
     throw new Error("Failed to fetch course details");
   }
+
   return response.json();
 };
