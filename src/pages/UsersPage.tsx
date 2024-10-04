7c232f9245cb74aaa0f847d7af45d074a8972c84
marcus pedersen <marcus@mpedersen.me>
2024-10-04 21:31:59 +0200

    Init

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -2,9 +2,8 @@ import { Box } from "@chakra-ui/react";
 import { useAuth } from "../context/authContext";
 
 const Users = () => {
-  const { user, logout } = useAuth();
-  if (user?.role !== "Teacher")
-    return <Box mt={20}>Only teachers have access to this page</Box>;
+  const { user } = useAuth();
+  if (user?.role !== "Teacher") return <Box mt={20}>Only teachers have access to this page</Box>;
 
   return <Box mt={20}>Users</Box>;
 };
