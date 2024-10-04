0d6dc8c0420ce3b63b4c9f634a2fad27c2009632
marcus pedersen <marcus@mpedersen.me>
2024-10-04 21:30:05 +0200

    Make navbar unified

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -42,34 +42,44 @@ export const Navbar = () => {
   };
 
   return (
-    <Box bg="teal.600" px={4} width="100%" position="fixed" top="0" zIndex="999">
+    <Box bg="primary.600" px={4} width="100%" position="fixed" top="0" zIndex="999">
       <Flex h={16} alignItems="center" justifyContent="space-between">
+        {/* LMS Title */}
         <Heading as="h1" size="lg" color="white" cursor="pointer" onClick={handleDashboardClick}>
           LMS
         </Heading>
+
         <Spacer />
+
+        {/* Right side of the Navbar */}
         <Flex alignItems="center">
+          {/* User Avatar and Name */}
           <Avatar name={user.userName} mr={4} />
           <Text color="white" mr={4}>
             {user.userName}
           </Text>
+
+          {/* Show actions only if the user is a teacher */}
           {user.role === "Teacher" && (
             <>
+              {/* Handle Users Button */}
               <Button
-                bg="yellow.400"
+                bg="accent.400"
                 color="black"
-                _hover={{ bg: "yellow.500" }}
+                _hover={{ bg: "accent.500" }}
                 onClick={handleUsersClick}
                 mr={4}>
                 Handle Users
               </Button>
+
+              {/* Create Menu */}
               <Menu>
                 <MenuButton
                   as={Button}
                   rightIcon={<ChevronDownIcon />}
-                  bg="yellow.400"
+                  bg="accent.400"
                   color="black"
-                  _hover={{ bg: "yellow.500" }}
+                  _hover={{ bg: "accent.500" }}
                   mr={4}>
                   Create
                 </MenuButton>
@@ -80,10 +90,12 @@ export const Navbar = () => {
               </Menu>
             </>
           )}
+
+          {/* Logout Button */}
           <Button
-            bg="yellow.400"
+            bg="neutral.400"
             color="black"
-            _hover={{ bg: "yellow.500" }}
+            _hover={{ bg: "neutral.500" }}
             onClick={handleLogout}>
             Logout
           </Button>
